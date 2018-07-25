847ffeaff470f77dde2892bcc982fe7c69288f35
Yash Rajgor <yash592@gmail.com>
2018-07-25 16:12:04 -0700

    maxChar1

--- maxchar/index.js
+++ maxchar/index.js
@@ -5,6 +5,24 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+var maxObj = {};
+
+
+function maxChar(str) {
+
+	for( let char of str ) {
+		if (!maxObj[char]) {
+			maxObj[char] = 1;
+		}
+
+		else {
+			maxObj[char]++;
+		}
+	}
+}
+
+console.log(maxObj)
+
+maxChar("Hello")
 
 module.exports = maxChar;
