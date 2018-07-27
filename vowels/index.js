aca32a16fc9638f450efd1f19e22dfda82a43fbc
Yash Rajgor <yash592@gmail.com>
2018-07-27 14:31:56 -0700

    vow

--- vowels/index.js
+++ vowels/index.js
@@ -3,10 +3,33 @@
 // used in a string.  Vowels are the characters 'a', 'e'
 // 'i', 'o', and 'u'.
 // --- Examples
-//   vowels('Hi There!') --> 3
+//   
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+
+	const arr = ['a', 'e', 'i', 'o', 'u'];
+
+	let wordOne = str.replace(/[^\w]/g, "").toLowerCase().split('');
+	console.log("Word: " + wordOne);
+	// arr.push(wordOne.split(''));
+	console.log(wordOne);
+
+	let vowelCount = 0;
+
+	for (var letters of wordOne) {
+		if(arr.includes(letters)) {
+			vowelCount++
+		}
+	}
+
+
+	console.log(vowelCount)
+}
+
+
+
+vowels('Hi There!')
 
 module.exports = vowels;
